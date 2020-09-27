429ddc6e2807caf08e707c74667e09558b8ae426
mehedi345 <mehedihasan5101220@gmail.com>
2020-09-27 16:45:12 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
+
+const password = '9LkZ6CvJsJYWQDc';
+
+const uri = "mongodb+srv://organicuser:9LkZ6CvJsJYWQDc@cluster0.ozwps.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+
+client.connect(err => {
+  const productCollection = client.db("organicdb").collection("products");
+
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({_id: ObjectId(req.params.id)})
+    .toArray((err,documents) => {
+      res.send(documents[0])
+    })
+  })
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productCollection.insert(product)
+    .then(result => {
+      console.log('data added successfully');
+      res.send('success');
+    })
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    productCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then( result => {
+      console.log(result);
+    })
+  })
+
+
+  
+});
+
+
+app.listen(3000)
