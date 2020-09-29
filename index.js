dbeaee8635d956313e5d2c921ade4da6ce1542bf
mehedi345 <mehedihasan5101220@gmail.com>
2020-09-28 19:02:35 -0700

    practicing

--- index.js
+++ index.js
@@ -40,14 +40,26 @@ client.connect(err => {
     productCollection.insert(product)
     .then(result => {
       console.log('data added successfully');
-      res.send('success');
+      res.redirect('/')
     })
   })
+
+  app.patch('/update/:id', (req, res) => {
+    
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+     {
+       $set:{price: req.body.price, quantity: req.body.quantity}
+     })
+     .then(result => {
+       res.send(result.modifiedCount > 0 )
+     })
+  })
+
 
   app.delete('/delete/:id', (req, res) => {
     productCollection.deleteOne({_id: ObjectId(req.params.id)})
     .then( result => {
-      console.log(result);
+      res.send(result.deletedCount > 0);
     })
   })
 
